23781d8fc6bad3086a655a34b4cc15bdfb5e289f
Ahmed Fasih <wuzzyview@gmail.com>
2020-01-09 23:51:54 -0500

    A bit more helpful usage printouts

--- index.js
+++ index.js
@@ -18,17 +18,20 @@ function num2fname(num) { return (num + 1).toString().padStart(3, '0'); }
   let cmds = [];
   for (const [num, o] of enumerate(srtObj)) {
     let seconds = (0.5 * (o.start + o.end) * 1e-3).toFixed(3);
-    let cmd = `ffmpeg -ss ${seconds} -i ${videoFname} -vframes 1 -q:v 2 ${num2fname(num)}.jpg`;
+    let cmd = `ffmpeg -ss ${seconds} -i '${videoFname}' -vframes 1 -q:v 2 ${num2fname(num)}.jpg`;
     cmds.push(cmd);
   }
   fs.writeFileSync('cmds.sh', cmds.join('\n'));
+
+  console.log(`Make sure FFMPEG is available, then:
+$ ./cmds.sh`);
 }
 {
   let mdlines = [];
   for (const [num, o] of enumerate(srtObj)) {
     let fname = num2fname(num);
     let textNolines = o.text.replace(/\n/g, '');
-    let quotedText = o.text.split('\n').map(x => '## ◊sent :: ? :: ' + x).join('\n\n');
+    let quotedText = o.text.split('\n').map(x => '## ' + x).join('\n\n');
     let block = `# ${fname}
 ![${textNolines}](${fname}.jpg)
 
@@ -38,7 +41,7 @@ ${quotedText}
     mdlines.push(block);
   }
   fs.writeFileSync('subs.md', mdlines.join('\n'));
-}
 
-console.log(`With a recent Pandoc you may now run
+  console.log(`With a recent Pandoc you may now run
 $ pandoc -f markdown-implicit_figures -i subs.md -o subs.html`);
+}
